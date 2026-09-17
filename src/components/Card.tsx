ba089adeb009a21db412693a398402dce093425e
Divyanshu Singh <divyanshu.singh@beehyv.com>
2026-09-17 13:11:50 +0530

    initial

---
+++ src/components/Card.tsx
@@ -0,0 +1,18 @@
+
+
+
+interface CardProps {
+    title: string;
+    description: string;
+}
+
+
+
+export default function Card({ title, description }: CardProps) {
+    return (
+        <div className="card">
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+    )
+}
